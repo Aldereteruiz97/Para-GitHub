b9885f7057ba0d743b42df9a5b55f0c6bf3adae0
Aldereteruiz97 <aldereteruiz97@gmail.com>
2022-11-29 17:29:46 -0300

    Para crear las variables dummy

---
+++ createDummies.py
@@ -0,0 +1,6 @@
+def createDummies(df, var_name):
+    dummy = pd.get_dummies(df[var_name], prefix=var_name)
+    df = data.drop(var_name, axis=1)
+    df = pd.concat([df, dummy], axis=1)
+
+    return df
