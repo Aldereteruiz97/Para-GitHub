48e50347b6bdb0d145cd5eb597b5a8f69487b3d2
Aldereteruiz97 <119321849+Aldereteruiz97@users.noreply.github.com>
2023-01-30 22:19:03 -0300

    Delete createDummies.py

--- createDummies.py
+++
@@ -1,6 +0,0 @@
-def createDummies(df, var_name):
-    dummy = pd.get_dummies(df[var_name], prefix=var_name)
-    df = data.drop(var_name, axis=1)
-    df = pd.concat([df, dummy], axis=1)
-
-    return df
